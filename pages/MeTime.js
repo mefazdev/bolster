029c7dd2ef1a072664c08872aca01cda9111b8ce
mefazdev <fazdev.com@gmail.com>
2022-03-16 14:48:59 +0530

    hghgh

--- pages/MeTime.js
+++ pages/MeTime.js
@@ -19,7 +19,7 @@ export default function MeTime() {
             &apos;Me Time&apos; is the conscious effort of taking (much needed)
             time-out to recharge your batteries. &apos;Me time&apos; can be spent in
             various ways and vary from person to person. It can be achieved in
-            different ways. Essentially, as long as you'&apos;e alone with your
+            different ways. Essentially, as long as you&apos;e alone with your
             thoughts and undertaking mindfulness during any activity, you&apos;re
             successfully spending some quality ME time. With the help of experts
             from different fields, Bolster foundation facilitates &apos;Me time&apos; in
