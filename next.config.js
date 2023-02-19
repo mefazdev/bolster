d50141da2b47caba1a784ef1a73c5ff22606b788
mefazdev <fazdev.com@gmail.com>
2023-02-19 15:18:19 +0530

    apply added

--- next.config.js
+++ next.config.js
@@ -8,3 +8,17 @@ module.exports = {
   },
   plugins: [],
 }
+
+
+module.exports = {
+  async redirects() {
+    return [
+      {
+        source: '/apply',
+        destination: 'https://docs.google.com/forms/d/1pozjGK8Nc8l3mpCdsFkN8SlvL6LcVm6FaOi4jkba-ws/edit?pli=1',
+        permanent: false,
+        basePath: false
+      },
+    ]
+  },
+};
